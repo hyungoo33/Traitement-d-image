2e4c5b5ef1a8671e4fc08b3bc77b0ddeb6eaaab8
hyungoo33 <57836507+hyungoo33@users.noreply.github.com>
2020-02-21 15:53:05 +0100

    Add files via upload

---
+++ app/src/main/rs/convolutionmoyenne.rs
@@ -0,0 +1,41 @@
+# pragma version (1)
+# pragma rs java_package_name ( com.qps.projettp1)
+# pragma rs fp relaxed
+
+
+
+rs_allocation in;
+
+int imageWidth;
+int imageHeight;
+
+int filterSize = 3;
+
+
+void root(const uchar4* in, uchar4* out,uint32_t x, uint32_t y){
+
+    float4 moy = 0;
+    const uchar4* kin = in - filterSize*(imageWidth+1);
+    for(int i = 0; i < filterSize; i++){
+        for(int j = 0; j < filterSize; j++){
+
+            moy += rsUnpackColor8888( kin[i*imageWidth + j]);
+        }
+    }
+    moy /= 9.0f;
+    moy.a = 1.0f;
+
+    *out = rsPackColorTo8888(moy);
+
+
+}
+
+void init(){
+
+}
+
+void setup(){
+
+    imageWidth = rsAllocationGetDimX(in);
+    imageHeight = rsAllocationGetDimY(in);
+}
